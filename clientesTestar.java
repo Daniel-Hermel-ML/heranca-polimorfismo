ebaf7789f18b817ed6213668e7da6354c7f50d5c
Daniel-Hermel-MELI <daniel.hermel@mercadolivre.com>
2023-01-17 21:09:22 -0300

    Entrega do exercicio de Herança e Polimorfismo - ajustado para envio Generation

--- clientesTestar.java
+++ clientesTestar.java
@@ -1,4 +1,74 @@
 package herancaPolimorfismoAtividade1;
+public abstract class Cliente {
+
+    private String nome;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void visualizar() {
+        System.out.print("Nome: " + nome+" ");
+    }
+    //////////////////
+    public class pessoaFisica extends Cliente{
+
+        private int cpf;
+
+        public pessoaFisica(String nome, int cpf) {
+            super(nome);
+            this.cpf = cpf;
+        }
+
+        public int getCpf() {
+            return cpf;
+        }
+
+        public void setCpf(int cpf) {
+            this.cpf = cpf;
+        }
+
+        @Override
+        public void visualizar() {
+            super.visualizar();
+            System.out.println("CPF: " + cpf);
+        }
+        ////////////////////////////
+        public class pessoaJuridica extends Cliente{
+
+            private int cnpj;
+
+            public pessoaJuridica(String nome, int cnpj) {
+                super(nome);
+                this.cnpj = cnpj;
+            }
+
+            public int getCnpj() {
+                return cnpj;
+            }
+
+            public void setCnpj(int cnpj) {
+                this.cnpj = cnpj;
+            }
+
+            @Override
+            public void visualizar() {
+                super.visualizar();
+                System.out.println("CNPJ: " + cnpj);
+            }
+    /////////////////////////////
 
 public class clientesTestar {
     public static void main(String[] args) {
